05b365eb3c9eb202985ba097cc241c5bf2c9555e
Adil Khan <khan01970@gmail.com>
2025-10-26 20:19:34 +0530

    BMS

---
+++ src/main/java/org/lld/PracticeQuestions/BookMyShow/Admin/ManageInfra.java
@@ -0,0 +1,22 @@
+package org.lld.PracticeQuestions.BookMyShow.Admin;
+
+import org.lld.PracticeQuestions.BookMyShow.Infrastructure.City;
+import org.lld.PracticeQuestions.BookMyShow.Infrastructure.Theatre;
+
+import java.util.List;
+import java.util.UUID;
+
+public class ManageInfra {
+    public void addNewTheatre(String theatreName, String cityName , Integer screens, List<City> cities){
+        Theatre theatre=new Theatre(UUID.randomUUID(),cityName,screens);
+        for(int i=0;i<cities.size();i++){
+            if(cities.get(i).getName().equals(cityName)){
+                cities.get(i).setTheatreList();
+            }
+        }
+    }
+
+    public City addNewCity(String cityName){
+        return new City(UUID.randomUUID(),cityName,null);
+    }
+}
--- src/main/java/org/lld/PracticeQuestions/BookMyShow/Main.java
+++ src/main/java/org/lld/PracticeQuestions/BookMyShow/Main.java
@@ -1,23 +1,40 @@
 package org.lld.PracticeQuestions.BookMyShow;
 
+import org.lld.PracticeQuestions.BookMyShow.Admin.ManageInfra;
+import org.lld.PracticeQuestions.BookMyShow.Infrastructure.City;
 import org.lld.PracticeQuestions.BookMyShow.Infrastructure.Theatre;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         //Create Users (User Registration)
-        User adil=new User(UUID.randomUUID(),"Adil",null);
-        User aditi=new User(UUID.randomUUID(),"Aditi",null);
+//        User adil=new User(UUID.randomUUID(),"Adil",null);
+//        User aditi=new User(UUID.randomUUID(),"Aditi",null);
+//
+//        //Create Infrastructure (Super Admin can add or Remove Theatre)
+//        Theatre pvr=new Theatre(UUID.randomUUID(),"PVR",4);
+//        Theatre inox=new Theatre(UUID.randomUUID(),"iNox",5);
+//
+//        //Creating Shows and Adding Movies (Admin can add or remove)
+//        Movie Avengers=new Movie(UUID.randomUUID(),"Avengers","Action","English");
+//        pvr.getScreenList().stream().filter(key->key.getShow()).
+//        Show marvel=new Show(Avengers,pvr.getScreenList())
 
-        //Create Infrastructure (Super Admin can add or Remove Theatre)
-        Theatre pvr=new Theatre(UUID.randomUUID(),"PVR",4);
-        Theatre inox=new Theatre(UUID.randomUUID(),"iNox",5);
+        //List of all cities in BMS
+        List<City> cities =new ArrayList<>();
+        //Create Object of Admin/ Infra
+        ManageInfra  manageInfra = new ManageInfra();
+        //Adding cities
+        City newCity=manageInfra.addNewCity("Bengaluru");
+        cities.add(newCity);
+        City newCity1=manageInfra.addNewCity("Delhi");
+        cities.add(newCity1);
 
-        //Creating Shows and Adding Movies (Admin can add or remove)
-        Movie Avengers=new Movie(UUID.randomUUID(),"Avengers","Action","English");
-        pvr.getScreenList().stream().filter(key->key.getShow()).
-        Show marvel=new Show(Avengers,pvr.getScreenList())
+        manageInfra.addNewTheatre();
+
     }
 }
